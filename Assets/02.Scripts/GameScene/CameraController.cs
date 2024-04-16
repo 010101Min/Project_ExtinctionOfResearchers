bc7e093ec01921788553bb851831112295ac6461
010101Min <117823790+010101Min@users.noreply.github.com>
2024-04-16 13:44:03 +0900

    그만...
    그만 수정하고싶다... 진짜로

--- Assets/02.Scripts/GameScene/CameraController.cs
+++ Assets/02.Scripts/GameScene/CameraController.cs
@@ -6,8 +6,9 @@ using static UnityEngine.GraphicsBuffer;
 public class CameraController : MonoBehaviour
 {
     public float rotateSpeed = 10f;
+    public GameObject redLight;
 
-    public void fCameraRotate() { StartCoroutine(CameraRotate()); }
+    public void fCameraRotate() { redLight.SetActive(false); StartCoroutine(CameraRotate()); }
     IEnumerator CameraRotate()
     {
         while (true)
